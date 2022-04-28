f854cf88e80d66d5f40ea7f760fd0223e36e257a
Lizeth <mendezlizeth084@gmail.com>
2022-04-28 09:04:50 -0700

    Registered Song Model with Admin Site + Completed Post Functionality for Song

--- Songs/serializers.py
+++ Songs/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
-from .models import Song, Songs
+from .models import Song
 
 
 
-class SongSerializer(serializers.ModelsSerializer()):
+class SongSerializer(serializers.ModelSerializer):
     class Meta:
         model = Song
         fields =['title', 'artist', 'album', 'release_date', 'genre']
--- Songs/views.py
+++ Songs/views.py
@@ -1,10 +1,24 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SongSerializer
+from .models import Song
+
+
 
 # Create your views here.
 
 
-@api_view(['GET'])
-
+@api_view(['GET', 'POST'])
 def songs_list(request):
-    return Response('Whatup')
+
+    if request.method == 'GET':
+        songs = Song.objects.all()
+        serializer = SongSerializer(songs, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == 'POST':
+        serializer = SongSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
